46b523fc5640895f2b858b18e292824dcff2b8a4
Shincy <shincyshnz@gmail.com>
2023-06-02 23:57:46 +0530

    edited index.js ==> commenetd app.use(userInfo)

--- Server/middlewares/userInfo.js
+++ Server/middlewares/userInfo.js
@@ -6,24 +6,24 @@ const userinfoLog = require("../LogUserInfo/userinfoLog.json");
 const userInfo = (req, res, next) => {
     console.log("Inside Application Level middleware");
 
-    // const newData = {
-    //     "url": req.url,
-    //     "method": req.method
-    // };
-    // userinfoLog.push(newData);
+    const newData = {
+        "url": req.url,
+        "method": req.method
+    };
+    userinfoLog.push(newData);
 
     // console.log(userData);
 
     /* -- Commenting because vercel do not support writing file,since vercel is serverless ---*/
 
-    // fs.writeFile("LogUserInfo/userinfoLog.json", JSON.stringify(userinfoLog), (err) => {
-    //     if (err) {
-    //         res.status(400).json({
-    //             message: err
-    //         });
-    //     }
-    //     console.log("User Data inserted");
-    // });
+    fs.writeFile("LogUserInfo/userinfoLog.json", JSON.stringify(userinfoLog), (err) => {
+        if (err) {
+            res.status(400).json({
+                message: err
+            });
+        }
+        console.log("User Data inserted");
+    });
 
     next();
 };
