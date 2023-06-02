00d4dca55cfa0e474b711b72faa50ed1b143670f
Shincy <shincyshnz@gmail.com>
2023-06-02 23:47:47 +0530

    commented express.static

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const errorHandle = require("./middlewares/errorHandle");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 //Application Level middleware
 app.use(userInfo);
